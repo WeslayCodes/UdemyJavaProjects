0b4dbe3a43be07969cd91668909618892248e7ac
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2024-03-05 14:21:02 -0600

    Removed output files, cleaned up InputCalculator.java

--- InputCalculator/src/InputCalculator.java
+++ InputCalculator/src/InputCalculator.java
@@ -7,19 +7,18 @@ public class InputCalculator {
 
     public static void inputThenPrintSumAndAverage() {
         Scanner stdIn = new Scanner(System.in);
-
         int sum = 0;
-        long average = 0;
+        int average = 0;
         int count = 0;
-        boolean isInt = true;
 
         while (stdIn.hasNextInt()) {
             sum += stdIn.nextInt();
             count++;
         }
 
-        if (count != 0)
-            average = Math.round((double) sum / count);
+        if (count != 0) {
+            average = (int) Math.round((double) sum / count);
+        }
 
         System.out.println("SUM = " + sum + " AVG = " + average);
         stdIn.close();
