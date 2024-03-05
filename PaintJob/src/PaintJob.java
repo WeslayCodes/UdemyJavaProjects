f3195821956091511a8148809aebbd8605cc04d1
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2024-03-05 17:02:49 -0600

    Cleaned up PaintJob.java

--- PaintJob/src/PaintJob.java
+++ PaintJob/src/PaintJob.java
@@ -3,22 +3,31 @@ public class PaintJob {
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0)
+        boolean invalidInput = width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0;
+
+        if (invalidInput) {
             return -1;
+        }
 
         return (int) Math.ceil((width * height - extraBuckets * areaPerBucket) / areaPerBucket);
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0)
+        boolean invalidInput = width <= 0 || height <= 0 || areaPerBucket <= 0;
+
+        if (invalidInput) {
             return -1;
+        }
 
         return (int) Math.ceil(width * height / areaPerBucket);
     }
 
     public static int getBucketCount(double area, double areaPerBucket) {
-        if (area <= 0 || areaPerBucket <= 0)
+        boolean invalidInput = area <= 0 || areaPerBucket <= 0;
+
+        if (invalidInput) {
             return -1;
+        }
 
         return (int) Math.ceil(area / areaPerBucket);
     }
