798b949eaa9aba3d1201624fc0b369861faa00c0
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2024-03-05 18:51:40 -0600

    Cleaned up Point.java

--- Point/src/Point.java
+++ Point/src/Point.java
@@ -5,8 +5,8 @@ public class Point {
     public Point() {}
 
     public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        setX(x);
+        setY(y);
     }
 
     public int getX() {
@@ -26,7 +26,7 @@ public class Point {
     }
 
     public double distance(int x, int y) {
-        return Math.sqrt((this.x-x)*(this.x-x)+(this.y-y)*(this.y-y));
+        return Math.sqrt((this.x-x) * (this.x-x) + (this.y-y) * (this.y-y));
     }
 
     public double distance() {
